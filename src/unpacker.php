2f3b1bfeaa56bc2f61bb9a6fba52f9a8e54af274
Peter Volkov <volkov_peter@mail.ru>
2014-03-17 16:59:01 +0400

    unpacker checks

--- src/unpacker.php
+++ src/unpacker.php
@@ -1,5 +1,13 @@
 <?php
 
+    define('PS_ERR_UNPACK_ARCHIVE', 'Ошибка при распаковке');
+    define('PS_NO_ZIP_MODULE', 'Для работы нужен PHP модуль "zip".');
+
+    $zipModuleVersion = phpversion('zip');
+    if (empty($zipModuleVersion)) {
+        die('PS_NO_ZIP_MODULE');
+    } 
+
     function unpack_pat() {
 		$archive_filename = 'pat.zip';
 		$archive = new ZipArchive;
@@ -14,9 +22,6 @@
 
     header('Location: /pat/index.php?controller=scanner');
     unlink(__FILE__);
-
-    
-
-    
+  
 ?>
  
